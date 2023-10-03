57fbefbf3e0f8c38a3b33ecbb6f968c33c776cf4
MadhanSmart <madhan.c5695@gmail.com>
2023-10-03 10:57:05 +0530

    Second commit

--- src/main/java/com/spring/securitypractice/Entity/UserInfo.java
+++ src/main/java/com/spring/securitypractice/Entity/UserInfo.java
@@ -25,7 +25,8 @@ public class UserInfo {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "user_roles", joinColumns = 
+	@JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
 	public UserInfo() {
